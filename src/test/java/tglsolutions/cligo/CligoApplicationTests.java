64a03e7fe91efee0de0e92cbcfe512dc1b9b6643
Jvlima22_ <josulima90@gmail.com>
2024-08-26 23:18:48 -0300

    first commit

---
+++ src/test/java/tglsolutions/cligo/CligoApplicationTests.java
@@ -0,0 +1,13 @@
+package tglsolutions.cligo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CligosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
